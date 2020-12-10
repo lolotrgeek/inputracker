15c5ad181cba1c2b5e41e76d1548e27ce5746fcd
lolotrgeek <lolotrgeek@gmail.com>
2020-12-10 07:11:50 -0600

    new maping for task learning

--- src/web-utils/parse.js
+++ src/web-utils/parse.js
@@ -4,9 +4,10 @@ const fs = require('fs')
 // add action space ids to key/modifiers 
 
 let keys = Object.assign({}, keymap.keys)
-let modifiers = Object.assign({}, keymap.modifiers)
+// let special = Object.assign({}, keymap.special)
+// let modifiers = Object.assign({}, keymap.modifiers)
 
-const JSONSTRING = JSON.stringify({ keys, modifiers})
+const JSONSTRING = JSON.stringify({ keys, })
 console.log(JSONSTRING)
 
 fs.writeFile('./newmap.json', JSONSTRING, ERR => {
